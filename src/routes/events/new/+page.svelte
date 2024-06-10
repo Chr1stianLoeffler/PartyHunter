f7a5b4e6c62df1fecf1e0088e2e861de68d39495
N/A <N/A>
2024-06-10 17:45:29 +0200

    Karte in new event (noch nicht funktional

--- src/routes/events/new/+page.svelte
+++ src/routes/events/new/+page.svelte
@@ -35,26 +35,108 @@
       errorMessage = "Es fehlen Informationen um ein Event zu erstellen"
     }
   };
+
+  import type { LocationPoint } from "../../../events/event";
+  import tt from "@tomtom-international/web-sdk-maps";
+  import "@tomtom-international/web-sdk-maps/dist/maps.css";
+  import { onMount } from "svelte";
+
+  let map: tt.Map;
+  let mapElement: HTMLElement;
+  let marker: tt.Marker;
+  onMount(() => {
+    map = tt.map({
+      key: 'OvTfqyf8PyjDAEEWc0evLsKYaVNdGvbL',
+      container: mapElement,
+      center: [8.53434, 49.474235],
+      zoom: 15.5,
+    });
+    marker = new tt.Marker().setLngLat([8.53434, 49.474235]).addTo(map);
+  });
+
+  let searchResults: LocationPoint[];
+  let query = "";
+  function queryTomTom() {
+    fetch(
+      `https://api.tomtom.com/search/2/search/${query}.json?minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=OvTfqyf8PyjDAEEWc0evLsKYaVNdGvbL`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        searchResults = data.results.map((element: any) => {
+          if(!element.poi.name) element.poi.name = element.address.freeformAddress;
+          return {
+            name: element.poi.name,
+            address: element.address.freeformAddress,
+            type: "tomtom",
+            id: element.id,
+            longitude: element.position.lon,
+            latitude: element.position.lat,
+          };
+        });
+      });
+  }
+
+  function setLL(longitude: number, latitude: number) {
+    if (marker) {
+      marker.remove();
+    }
+    if (map) {
+      map.setCenter([longitude, latitude]);
+      map.setZoom(14);
+      marker = new tt.Marker().setLngLat([longitude, latitude]).addTo(map);
+    }
+  }
 </script>
 
 <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10">
   <h2 class="text-2xl text-gray-700 font-bold mb-6">Neues Event erstellen</h2>
   <form on:submit={handleSubmit}>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Event Name</label>
-      <input bind:value={eventName} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Event Name">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="name"
+        >Event Name</label
+      >
+      <input
+        bind:value={eventName}
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="name"
+        type="text"
+        placeholder="Event Name"
+      />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="date">Event Datum</label>
-      <input bind:value={eventDate} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="date">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="date"
+        >Event Datum</label
+      >
+      <input
+        bind:value={eventDate}
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="date"
+        type="date"
+      />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="location">Event Ort</label>
-      <input bind:value={eventLocation} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="location" type="text" placeholder="Event Ort">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="location"
+        >Event Ort</label
+      >
+      <input
+        bind:value={eventLocation}
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="location"
+        type="text"
+        placeholder="Event Ort"
+      />
     </div>
     <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Event Beschreibung</label>
-      <textarea bind:value={eventDescription} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" placeholder="Event Beschreibung"></textarea>
+      <label
+        class="block text-gray-700 text-sm font-bold mb-2"
+        for="description">Event Beschreibung</label
+      >
+      <textarea
+        bind:value={eventDescription}
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="description"
+        placeholder="Event Beschreibung"
+      ></textarea>
     </div>
     <div class="flex items-center justify-between">
       <button class="btn text-gray-700 btn-primary" type="submit">Event erstellen</button>
@@ -64,4 +146,52 @@
   {#if errorMessage}
     <div class="error-message text-gray-700">{errorMessage}</div>
   {/if}
+</div>
+
+<div class="grid grid-cols-12 space-x-2">
+  <div class="map col-span-8 w-[50rem] h-[30rem]" bind:this={mapElement}></div>
+  <div class="col-span-4 space-y-2 w-[25rem] h-[30rem]">
+    <div class="grid grid-cols-4 space-x-2">
+      <div class="col-span-3">
+        <label for="search">Suche einen Ort</label>
+        <input
+          id="search"
+          type="text"
+          class="block w-full"
+          bind:value={query}
+          on:keydown={(e) => {
+            if (e.key == "Enter") {
+              queryTomTom();
+            }
+          }}
+        />
+      </div>
+      <button
+        on:click={() => {
+          queryTomTom();
+        }}
+        class="col-span-1 bg-blue-500 p-2 rounded-lg">Suche</button
+      >
+    </div>
+    <div class="overflow-y-auto h-full space-y-2">
+      {#if searchResults}
+        {#each searchResults as searchResult}
+          <div
+            class="bg-white rounded p-2 grid grid-cols-6 space-y-1 items-center"
+          >
+            <div class="col-span-4">
+              <p class="text-lg">{searchResult.name}</p>
+              <p class="text-base">{searchResult.address}</p>
+            </div>
+            <button
+              class="col-span-2 bg-blue-500 p-y-2 rounded-lg w-24 h-12"
+              on:click={() =>
+                setLL(searchResult.longitude, searchResult.latitude)}
+              >Auswählen</button
+            >
+          </div>
+        {/each}
+      {/if}
+    </div>
+  </div>
 </div>
